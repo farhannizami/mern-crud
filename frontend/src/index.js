fa474d14812c3c7f3cead06f8aec424ad36bb601
Farhan <farhannizami0612@gmail.com>
2024-05-13 23:05:34 +0600

    AuthContext added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { NoteContextProvider } from './context/NoteContext';
+import { AuthContextProvider } from './context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <NoteContextProvider>
-      <App />
-    </NoteContextProvider>
+    <AuthContextProvider>
+      <NoteContextProvider>
+        <App />
+      </NoteContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
